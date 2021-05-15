3b20c867e3ef5d62b3743437a78543a84ce3df7f
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2021-05-15 21:05:20 +0300

    Update date.py

--- date.py
+++ date.py
@@ -18,3 +18,13 @@ def change_date_format(dt):
 # dt1 = '31.12.20'
 # print("Original date in DD-MM-YY Format: ",dt1)
 print("New date in DD-MM-YYYY Format: ",change_date_format(file_))
+
+
+
+import re
+
+def change_date_format(dt):
+        return re.sub(r'(0?[1-9]|[12]\d|30|31)[.](0?[1-9]|1[0-2])[.](\d{4}|\d{2})', '\\1-\\2-\\3', dt)
+# dt1 = '31.12.20'
+# print("Original date in DD-MM-YY Format: ",dt1)
+print("New date in DD-MM-YYYY Format: ",change_date_format(file_))
