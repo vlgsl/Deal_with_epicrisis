0f02ac75d40115808eae6de70ce64905f0595e8c
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2021-09-22 14:05:30 +0300

    initial commit

---
+++ sql/createTables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE fibrinogen VALUES (
+    ID_CASE_REPORT INT,
+    Bitrthday date
+    FibrinogenValue FLOAT,
+    FibrinogenDate date
+    );
+
+CREATE TABLE biochemical_test VALUES (
+    ID_CASE_REPORT INT,
+    Bitrthday date,
+    CrpValue FLOAT,
+    CrpDate date,
+    CreatininValue FLOAT,
+    CreatininDate date,
+    ASTValue FLOAT,
+    ASTDate date,
+    ALTValue FLOAT,
+    ALTDate date,
+    LDHValue FLOAT,
+    LDHDate date,
+    GlucoseValue FLOAT,
+    GlucoseDate date
+    );
+
